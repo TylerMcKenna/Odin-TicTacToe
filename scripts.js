f3ca1d65570787630d07f637b6c646fd9893e63f
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-03-19 11:13:34 -0500

    Create Factory Functions for Cells and Players
    Both work in the console.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
 const para = document.createElement("p");
 para.textContent = "Hello, world!"
 document.querySelector("body").appendChild(para);
+
+function cell() {
+    let value = 0;
+    
+    const getValue = () => value;
+
+    const setPlayer = (player) => {
+        value = player.getToken()
+    };
+
+    return { getValue, setPlayer };
+}
+
+
+function player(token, name = "Player") {
+    let userToken;
+    const userName = name;
+
+    if (token === "X") {
+        userToken = 1;
+    } else if (token === "O") {
+        userToken = 2
+    } else {
+        console.log("Please enter a valid token. This would be either X or O.")
+        return null;
+    }
+
+    const getToken = () => userToken;
+    const getName = () => userName;
+
+    return { getToken, getName };
+}
